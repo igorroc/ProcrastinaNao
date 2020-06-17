d54c5a1e5882bfda6f3aeaede0c51b74f902b9b8
IgorRoc <igor_roc@hotmail.com.br>
2020-06-17 02:07:44 -0300

    Correção de bug (UserInfo)

--- commands/userinfo.js
+++ commands/userinfo.js
@@ -10,26 +10,28 @@ module.exports.run = async (bot, message, args) => {
     .setTitle("Informações de Usuário")
     .setThumbnail(message.guild.iconURL)
 
-    if(args){
+    if(args.length > 0){
         let user = message.mentions.users.first()
         if(!user){
             message.channel.send(`Ocorreu um erro ao encontrar o usuário mencionado.`)
             console.log(`↳ Usuário "${args}" não encontrado, operação cancelada.`)
             return
         }
+        
         uEmbed.setAuthor(`Info ${user.username}`, user.displayAvatarURL)
-        .addField("**Nome:**", `${user.username}`, true)
-        .addField("**Tag:**", `${user.discriminator}`, true)
-        .addField("**ID:**", `${user.id}`, true)
-        .addField("**Status:**", `${user.presence.status}`, true)
-        .addField("**Desde:**", `${user.createdAt}`, true)
+        .addField("**Nome:**", user.username, true)
+        .addField("**Tag:**", user.discriminator, true)
+        .addField("**ID:**", user.id, true)
+        .addField("**Status:**", user.presence.status, true)
+        .addField("**Desde:**", user.createdAt.toDateString(), true)
+        
     }else{
         uEmbed.setAuthor(`Info ${message.author.username}`, message.author.displayAvatarURL)
-        .addField("**Nome:**", `${message.author.username}`, true)
-        .addField("**Tag:**", `${message.author.discriminator}`, true)
-        .addField("**ID:**", `${message.author.id}`, true)
-        .addField("**Status:**", `${message.author.presence.status}`, true)
-        .addField("**Desde:**", `${message.author.createdAt}`, true)
+        .addField("**Nome:**", message.author.username, true)
+        .addField("**Tag:**", message.author.discriminator, true)
+        .addField("**ID:**", message.author.id, true)
+        .addField("**Status:**", message.author.presence.status, true)
+        .addField("**Desde:**", message.author.createdAt.toDateString(), true)
     }
     
     
