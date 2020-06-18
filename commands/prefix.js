dfe8c0d11b4befa696181e2ace98362666721132
IgorRoc <igor_roc@hotmail.com.br>
2020-06-17 22:41:36 -0300

    Pequenas alterações

--- commands/prefix.js
+++ commands/prefix.js
@@ -10,20 +10,22 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("ADMINISTRATOR")){
         message.reply('Você não é digno de realizar esse comando!')
         console.log(`↳ Acesso negado para "${message.author.username}"`)
-    }else{
-        let [novoPrefix] = args
-        if(novoPrefix == "reset"){
-            await config.set('prefix', '.').write()
-            console.log(`↳ Prefixo resetado para "."`)
-            message.channel.send(`\`\`\`md\n# Prefix do servidor resetado para:\n.\`\`\``)
-        }else if(args[1]){
-            message.channel.send(`\`\`\`md\n# Não é possivel adicionar um prefixo com um espaço em branco.\`\`\``);
-        }else{
-            await config.set('prefix', novoPrefix).write()
-            console.log(`↳ Prefix alterado para "${novoPrefix}"`)
-            message.channel.send(`\`\`\`md\n# Prefix do servidor alterado para:\n${novoPrefix}\n\`\`\``);
-        }
+        return
     }
+    
+    let [novoPrefix] = args
+    if(novoPrefix == "reset"){
+        await config.set('prefix', '.').write()
+        console.log(`↳ Prefixo resetado para "."`)
+        message.channel.send(`\`\`\`md\n# Prefix do servidor resetado para:\n.\`\`\``)
+    }else if(args[1]){
+        message.channel.send(`\`\`\`md\n# Não é possivel adicionar um prefixo com um espaço em branco.\`\`\``);
+    }else{
+        await config.set('prefix', novoPrefix).write()
+        console.log(`↳ Prefix alterado para "${novoPrefix}"`)
+        message.channel.send(`\`\`\`md\n# Prefix do servidor alterado para:\n${novoPrefix}\n\`\`\``);
+    }
+    
     bot.user.setActivity(`| Digite ${config.get('prefix').value()}help para ajuda | Criado por Igor Rocha |`, {type: 'LISTENING'})
 
     
