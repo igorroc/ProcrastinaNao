c3628cc8af16b1813bcc6c1b061c6d72b9070674
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-07-19 11:38:19 -0300

    [fix]: discord.js v12

--- commands/msg.js
+++ commands/msg.js
@@ -4,27 +4,32 @@ const Discord = require("discord.js")
 module.exports.run = async (bot, message, args) => {
     console.log(`\n■▶ [LOGS] ⇥ Usuário "${message.author.username}" usou o comando MSG`)
 
-    let [canal] = args
-    if(canal.length > 18){
-        canal = canal.slice(2, 20)
-    }
-
     if(!message.member.hasPermission("ADMINISTRATOR")){
         message.reply('Você não é digno de realizar esse comando!')
         console.log(`↳ Acesso negado para "${message.author.username}."`)
     }
     
-    if(!bot.channels.get(canal)){
-        message.channel.send("``Canal não encontrado.``").then(m =>{
-            const contagem = setInterval(() => {
-                m.delete()
-                clearInterval(contagem)
-            }, 3000)
+    let [canal] = args
+    if(!canal){
+        return message.channel.send("Faltam argumentos.").then(m =>{
+            m.delete( {timeout: 3000} )
+        })
+    }
+    if(canal.length > 18){
+        canal = canal.slice(2, 20)
+    }
+
+    if(!bot.channels.cache.get(canal)){
+        message.channel.send("Canal não encontrado.").then(m =>{
+            m.delete( {timeout: 3000} )
         })
         console.log(`↳ Canal "${canal}" não encontrado.`)
     }else{
         let msg = args.slice(1).join(" ");
-        bot.channels.get(canal).send(msg)
+        if(!msg){
+            return message.channel.send("Envie uma mensagem")
+        }
+        bot.channels.cache.get(canal).send(msg)
     }
     message.delete();
         
@@ -36,6 +41,5 @@ module.exports.config = {
     description: "Envia a mensagem desejada para o canal especificado!",
     usage: ".msg [id_do_canal] [mensagem]",
     accessableby: "Moderadores",
-    noalias: "Sem variações",
-    aliases: ["say"]
+    aliases: ["say", "mensagem"]
 }
