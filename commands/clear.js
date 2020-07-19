5b2f7315f17612e8d19d82dcc25ed5ce1299c079
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-07-19 11:00:10 -0300

    [fix]: update para discord.js v12 apenas apaga ate 100 mensagens

--- commands/clear.js
+++ commands/clear.js
@@ -1,4 +1,5 @@
-const Discord = require("discord.js")
+const Discord = require("discord.js");
+const { default: fetch } = require("node-fetch");
 
 
 
@@ -6,33 +7,23 @@ module.exports.run = async (bot, message, args) => {
     console.log(`\n■▶ [LOGS] ⇥ Usuário "${message.author.username}" usou o comando Clear`)
 
     function emojiStr (id){
-        return bot.emojis.get(id).toString();
+        return bot.emojis.cache.get(id).toString();
     }
 
-    if(!message.member.hasPermission("ADMINISTRATOR")){ // se nao tem permissao para apagar
+    if(!message.member.hasPermission("MENAGE_MESSAGES")){ // se nao tem permissao para apagar
         message.reply('Você não é digno de realizar esse comando!')
         console.log(`↳ Acesso negado para "${message.author.username}"`)
     }
     else{
-        let confirmacao = await message.channel.send(`${emojiStr("722456385098481735")} Apagando mensagens...`)
-        const user = message.mentions.users.first();
-        // Parse Amount
-        const amount = !!parseInt(message.content.split(' ')[1]) ? parseInt(message.content.split(' ')[1]) : parseInt(message.content.split(' ')[2])
-        if (!amount) return message.reply('Você deve indicar uma quantidade!');
-        if (!amount && !user) return message.reply('Você deve indicar um usuario e/ou uma quantidade!');
-        // Fetch 100 messages (will be filtered and lowered up to max amount requested)
-        message.channel.fetchMessages({
-            limit: 100,
-            }).then((messages) => {
-                if (user) {
-                    const filterBy = user ? user.id : bot.user.id;
-                    messages = messages.filter(m => m.author.id === filterBy).array().slice(0, amount);
-                }
-                message.channel.bulkDelete(messages).catch(error => console.log(error.stack));
-                console.log(`↳ ${amount} mensagens apagadas`)
-            });
-        await confirmacao.delete()
-        await message.delete()
+        let valor = parseInt(args[0], 10);
+        if(!valor || valor < 1 || valor > 100)
+            return message.channel.send("Mande um valor de 1 até 100 para eu deletar")
+        
+        const fetched = await message.channel.messages.fetch({limit: valor + 1});
+        message.channel.bulkDelete(fetched)
+        .catch(e => console.log(`Erro ao apagar mensagens: ${e}`))
+        message.channel.send(`${args[0]} mensagens apagadas!`)
+            .then(m => m.delete({ timeout: 5000 }))
     }
 }
 
@@ -40,7 +31,7 @@ module.exports.run = async (bot, message, args) => {
 module.exports.config = {
     name: "clear",
     description: "Apaga as ultimas mensagens do servidor!",
-    usage: ".clear [quantidade] (usuario)",
+    usage: ".clear [quantidade]",
     accessableby: "Moderadores",
     aliases: ["c"]
 }
