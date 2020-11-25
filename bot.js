445aed2da3945c1fc46aad1ea234a7f43f94fdde
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-11-25 13:48:44 -0300

    [quick]: Retornaram as mensagem de reinicio

--- bot.js
+++ bot.js
@@ -47,17 +47,17 @@ bot.once("ready", () => {
     
     const log = bot.channels.cache.get('722274694535053317')
 
-    // let reload = log.send(`${loading} Reiniciando...`).then(async m1 => {
-    //     await m1.edit(`✅ Reiniciado!`).then(async m1 => {
-    //         await m1.delete().catch( () => console.log(`↳ ⚠️ Erro ao deletar a mensagem`) )
-    //     })
-    //     .catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
-    // }).catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
+    let reload = log.send(`${loading} Reiniciando...`).then(async m1 => {
+        await m1.edit(`✅ Reiniciado!`).then(async m1 => {
+            await m1.delete().catch( () => console.log(`↳ ⚠️ Erro ao deletar a mensagem`) )
+        })
+        .catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
+    }).catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
     
-    // let starting = log.send(`${loading}`).then(async m2 => {
-    //         await m2.edit(`✅ Bot iniciado, total de \`${memberCount}\` participantes`)
-    //             .catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
-    //     }).catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
+    let starting = log.send(`${loading}`).then(async m2 => {
+            await m2.edit(`✅ Bot iniciado, total de \`${memberCount}\` participantes`)
+                .catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
+        }).catch( () => console.log(`↳ ⚠️ Erro ao editar a mensagem`) )
 
     if(config.online == true){
         bot.user.setStatus('online')
