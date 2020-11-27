27f2aa52774746edeeaac6955f248b514d1bd0bf
Igor Lima Rocha <igor_roc@hotmail.com.br>
2020-11-27 16:51:04 -0300

    [style]: Divisao dos comandos por "," inves de "|"

--- commands/help.js
+++ commands/help.js
@@ -6,7 +6,7 @@ const prefix = botconfig.prefix
 module.exports.run = async (bot, message, args) => {
     console.log(`\n■▶ [LOGS] ⇥ Usuário '${message.author.username}' usou o comando Help`)
 
-    if(args[0] == "help") return message.channel.send(`Use \`${prefix}help\` invés disso.`)
+    if(args[0] == "help") args[0] = null
 
     if(args[0]) {
         let command = args[0];
@@ -15,32 +15,33 @@ module.exports.run = async (bot, message, args) => {
             
             var SHembed = new Discord.MessageEmbed()
             .setColor(colours.yellow)
-            .setAuthor(bot.user.username, message.guild.iconURL)
-            .setThumbnail(bot.user.displayAvatarURL)
-            .setDescription(`O prefixo do bot é: \` ${prefix} \`\n\n**Comando:** ${command.config.name}\n**Descrição:** ${command.config.description || "Sem descrição"}\n**Uso:** ${command.config.usage || "Sem uso"}\n**Acessivel para:** ${command.config.accessableby || "Membros"}\n**Variações:** ${command.config.noalias || command.config.aliases}`)
+            .setAuthor(bot.user.username, message.guild.iconURL)    
+            .setDescription(`\n**Comando:** ${command.config.name}\n**Descrição:** ${command.config.description || "Sem descrição"}\n**Uso:** ${command.config.usage || "Sem uso"}\n**Acessivel para:** ${command.config.accessableby || "Membros"}\n**Variações:** ${command.config.noalias || command.config.aliases}`)
             message.channel.send(SHembed);
-        }}
+        }else{
+            message.channel.send(`Comando \`${command}\` não encontrado`)
+            console.log(`↳ Comando '${command}' não encontrado`)
+        }
+    }
 
     if(!args[0]) {
-        let comandosSimples = Array.from(bot.commands.filter(c => c.config.accessableby === 'Membros' )).join(" `|` ")
+        let comandosSimples = Array.from(bot.commands.filter(c => c.config.accessableby === 'Membros' )).join("`, `")
         comandosSimples = comandosSimples.split("[object Object]").toString().replace(/,,/g, '')
         
-        let comandosAdmin = Array.from(bot.commands.filter(c => c.config.accessableby === 'Moderadores' )).join(" `|` ")
+        let comandosAdmin = Array.from(bot.commands.filter(c => c.config.accessableby === 'Moderadores' )).join("`, `")
         comandosAdmin = comandosAdmin.split("[object Object]").toString().replace(/,,/g, '')
         
         let Sembed = new Discord.MessageEmbed()
         .setColor(colours.yellow)
         .setAuthor(bot.user.username, message.guild.iconURL)
-        .setThumbnail(bot.user.displayAvatarURL)
-        .setTimestamp()
-        .setDescription(`Esses são os comandos disponíveis para o Bot Anti-Procrastinador!\nO prefixo do bot é: \`${prefix}\``)
-        .addField(`Comandos:`, "` " + comandosSimples + " `")
+        .setDescription(`Esses são os comandos disponíveis para o Bot Anti-Procrastinador!`)
+        .addField(`Comandos:`, "`" + comandosSimples + "`")
         
         if(message.member.hasPermission("ADMINISTRATOR")){
-            Sembed.addField("Comandos Especiais:", "` " + comandosAdmin + " `")
+            Sembed.addField("Comandos Especiais:", "`" + comandosAdmin + "`")
         }
         Sembed.addField("Para mais informações", `digite \`${prefix}help [comando]\``)
-        .setFooter(`${bot.user.username} | Comandos: ${bot.commands.size}`, bot.user.displayAvatarURL)
+        .setFooter(`${bot.user.username} | Comandos: ${bot.commands.size}`, bot.user.displayAvatarURL())
         message.channel.send(Sembed)
     }
 }
