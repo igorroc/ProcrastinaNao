b6cb7871b15c3e832f706fc9b73aa635ddb3c4df
IgorRoc <igor_roc@hotmail.com.br>
2021-03-23 11:44:20 -0300

    [new]: Comando de expulsão de usuário

---
+++ commands/kick.js
@@ -0,0 +1,47 @@
+const Discord = require("discord.js")
+
+
+module.exports.run = async (bot, message, args) => {
+    console.log(`\n■▶ [LOGS] ⇥ Usuário '${message.author.username}' usou o comando Expulsar`)
+
+    if(!message.member.hasPermission("ADMINISTRATOR")){
+        message.reply('Você não é digno de realizar esse comando!')
+        console.log(`↳ Acesso negado para '${message.author.username}'`)
+    }
+    
+    let user = message.mentions.users.first()
+    if(!user){
+        message.channel.send(`Ocorreu um erro ao encontrar o usuário mencionado.`)
+        console.log(`↳ Usuário "${args[0]}" não encontrado, operação cancelada.`)
+        return
+    }
+
+    let razao = args.slice(1).join(" ")
+    if(!razao){
+        return message.channel.send("Indique uma razão para a remoção do usuário.")
+    }
+
+
+    const embed = new Discord.MessageEmbed()
+        .setColor('#E3863F')
+        .setTitle(`Expulsão de ${user.username}`)
+        .setDescription(`Usuário: ${user}\nExpulso por: ${message.author}`)
+        .addFields(
+            { name: 'Motivo:', value: razao, inline: false },
+        )
+        .setFooter('Hora da expulsão:')
+        .setTimestamp()
+    
+    message.channel.send(embed);
+
+    // user.kick({reason: razao})
+}
+
+
+module.exports.config = {
+    name: "expulsar",
+    description: "Expulsa o usuário marcado!",
+    usage: ".expulsar [@pessoa] [razão]",
+    accessableby: "Moderadores",
+    aliases: ["kick"]
+}
