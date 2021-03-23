fd838175ce361320dc8b34a46c0d12f89e7a1fd8
IgorRoc <igor_roc@hotmail.com.br>
2021-03-23 13:08:47 -0300

    [fix]: Comando funcional

--- commands/kick.js
+++ commands/kick.js
@@ -7,12 +7,19 @@ module.exports.run = async (bot, message, args) => {
     if(!message.member.hasPermission("KICK_MEMBERS")){
         message.reply('Você não é digno de realizar esse comando!')
         console.log(`↳ Acesso negado para '${message.author.username}'`)
+        return
     }
     
     let user = message.mentions.users.first()
+    let member = message.mentions.members.first()
     if(!user){
         message.channel.send(`Ocorreu um erro ao encontrar o usuário mencionado.`)
         console.log(`↳ Usuário "${args[0]}" não encontrado, operação cancelada.`)
+        return
+    }
+    if(!member.kickable){
+        message.channel.send(`Esse usuário não pode ser expulso.`)
+        console.log(`↳ Usuário "${args[0]}" não pode ser expulso, operação cancelada.`)
         return
     }
 
@@ -21,20 +28,22 @@ module.exports.run = async (bot, message, args) => {
         return message.channel.send("Indique uma razão para a remoção do usuário.")
     }
 
-
-    const embed = new Discord.MessageEmbed()
-        .setColor('#E3863F')
-        .setTitle(`Expulsão de ${user.username}`)
-        .setDescription(`Usuário: ${user}\nExpulso por: ${message.author}`)
-        .addFields(
-            { name: 'Motivo:', value: razao, inline: false },
-        )
-        .setFooter('Hora da expulsão:')
-        .setTimestamp()
-    
-    message.channel.send(embed);
-
-    // user.kick({reason: razao})
+    member.kick({reason: razao})
+            .then(() => {
+                console.log("Usuário expulso")
+                
+                const embed = new Discord.MessageEmbed()
+                    .setColor('#E3863F')
+                    .setTitle(`Expulsão de ${user.username}`)
+                    .setDescription(`Usuário: ${user}\nExpulso por: ${message.author}`)
+                    .addFields(
+                        { name: 'Motivo:', value: razao, inline: false },
+                    )
+                    .setFooter('Hora da expulsão:')
+                    .setTimestamp()
+                
+                message.channel.send(embed);
+        }).catch(console.error())
 }
 
 
